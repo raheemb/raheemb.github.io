b56f5d81f6ad22133b1aaba3ece8fd6a067a3554
raheemb <raheemb@princeton.edu>
2019-11-29 16:24:48 -0500

    starfield

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,30 +1,32 @@
+// Adapted from Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/17WoOqgXsRM
+
 var canvas;
-let grow = 0;
-let sign = 1;
+let stars = [];
+let speed = 5;
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  background(255);
-  fill(67,204,203); 
-  noStroke();
   canvas.position(0,0);
   canvas.style('z-index','-1');
+  background(255);
+  noStroke();
+  for (let i = 0; i < 400; i++) {
+    stars[i] = new Star();
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function mouseClicked() {
-  sign = -sign;
-}
-
 function draw() {
-  background(255); 
-  ellipse(mouseX, mouseY, grow);
-  grow = grow + sign;
-  if (grow > 50 || grow < -100) {
-    sign = -sign;
+  background(255);
+  translate(width / 2, height / 2);
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
   }
 }
-
